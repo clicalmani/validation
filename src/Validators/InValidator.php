429f045c4ce4f6ec9580ce6ffca55a3f6164d96e
@clicalmani <honorable85@gmail.com>
2025-02-11 10:23:11 +0100

    Add EnumValidator class extending Validator for 'in' validation

---
+++ src/Validators/InValidator.php
@@ -0,0 +1,9 @@
+<?php
+namespace Clicalmani\Validation\Validators;
+
+use Clicalmani\Validation\Validators\EnumValidator as Validator;
+
+class EnumValidator extends Validator
+{
+    protected string $argument = 'in';
+}
